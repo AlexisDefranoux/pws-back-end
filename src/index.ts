4d1f4d53cecc55aabf14d4b2af06e2edbc16dfcd
ElectrozDen <sibaki.denys@gmail.com>
2020-02-13 05:40:48 +0100

    add typescript and tslint

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,41 @@
+import express, { Express } from 'express';
+import path from 'path';
 
-const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 const ParseDashboard = require('parse-dashboard');
-const path = require('path');
 
 
-const SERVER_PORT = process.env.PORT || 8080;
-const SERVER_HOST = process.env.HOST || 'localhost';
-const APP_ID = process.env.APP_ID || 'parse-server-webplugin';
-//C'EST SECRET CA, OK ?!!!!!!!!
-const MASTER_KEY = process.env.MASTER_KEY || 'UQdRmQLQwxV5NT8L6a74kqF8aPqIM9F';
-const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/parse-server-webplugin';
-const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
-const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH || 'denys:isagod';
+const SERVER_PORT: string | number = process.env.PORT || 8080;
+const SERVER_HOST: string = process.env.HOST || 'localhost';
+const APP_ID: string = process.env.APP_ID || 'parse-server-webplugin';
+// C'EST SECRET CA, OK ?!!!!!!!!
+const MASTER_KEY: string = process.env.MASTER_KEY || 'UQdRmQLQwxV5NT8L6a74kqF8aPqIM9F';
+const DATABASE_URI: string = process.env.DATABASE_URI || 'mongodb://localhost:27017/parse-server-webplugin';
+const IS_DEVELOPMENT: boolean = process.env.NODE_ENV !== 'production';
+const DASHBOARD_AUTH: string = process.env.DASHBOARD_AUTH || 'denys:isagod';
 
-const app = express();
 
+const app: Express = express();
 
 const parseServerAPI = new ParseServer({
     databaseURI: DATABASE_URI,
-    cloud: path.resolve(__dirname, 'cloud.js'),
     appId: APP_ID,
     masterKey: MASTER_KEY,
     serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`
 });
 
-
-app.get("/", function (req, res) {
+app.get("/",  (req, res) => {
     res.end("IS_DEVELOPMENT => " + IS_DEVELOPMENT);
 });
 
 app.use('/parse', parseServerAPI);
 
 
+
 if(IS_DEVELOPMENT) {
-    let users;
+    let users: {user: string; pass: string}[] = [];
     if (DASHBOARD_AUTH) {
-        const [user, pass] = DASHBOARD_AUTH.split(':');
+        const [user, pass]: string[] = DASHBOARD_AUTH.split(':');
         users = [{user, pass}];
     }
     const dashboard = ParseDashboard({
@@ -50,6 +49,7 @@ if(IS_DEVELOPMENT) {
     }, IS_DEVELOPMENT);
     app.use("/dashboard", dashboard);
 }
+
 
 
 app.listen(SERVER_PORT, () => console.log(
