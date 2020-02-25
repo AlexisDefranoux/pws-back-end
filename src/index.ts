67cb6f4bb4c189e6d7991fc35241ec0ed652b7fc
KienanBachwa <kienan.bachwa@etu.unice.fr>
2020-02-25 10:37:19 +0100

    Added comment input

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import express, { Express } from 'express';
+import express, {Express} from 'express';
 import cors from 'cors';
 import path from 'path';
 
 const ParseServer = require('parse-server').ParseServer;
 const ParseDashboard = require('parse-dashboard');
 
-
 const SERVER_PORT: string | number = process.env.PORT || 8080;
 const SERVER_HOST: string = process.env.HOST || 'localhost';
 const APP_ID: string = process.env.APP_ID || 'parse-server-webplugin';
-// C'EST SECRET CA, OK ?!!!!!!!!
 const MASTER_KEY: string = process.env.MASTER_KEY || 'UQdRmQLQwxV5NT8L6a74kqF8aPqIM9F';
 const DATABASE_URI: string = process.env.DATABASE_URI || 'mongodb://localhost:27017/parse-server-webplugin';
 const IS_DEVELOPMENT: boolean = process.env.NODE_ENV !== 'production';
-const DASHBOARD_AUTH: string = process.env.DASHBOARD_AUTH || 'denys:isagod';
+const DASHBOARD_AUTH: string = process.env.DASHBOARD_AUTH || 'parseuser:parsepwd';
 
 
 const app: Express = express();
@@ -27,10 +25,41 @@ const parseServerAPI = new ParseServer({
     maxUploadSize: '50mb'
 });
 
+
 app.use(cors());
 
-app.get("/",  (req, res) => {
+app.get("/", (req, res) => {
     res.end("IS_DEVELOPMENT => " + IS_DEVELOPMENT);
+});
+
+app.get("/forkplugin", (req, res) => {
+    const idPlugin = req.query.idPlugin;
+    const idUser = req.query.idUser;
+
+    let user: any;  // User
+    const queryUse = new Parse.Query(Parse.Object.extend("User"));
+    queryUse.get(idUser).then(queryUserResult => {
+        user = queryUserResult;
+    });
+
+    const query = new Parse.Query(Parse.Object.extend("Plugin"));
+    query.get(idPlugin).then(queryResult => {
+        const cloned = queryResult.clone();
+        delete cloned.attributes.objectId;
+        cloned.set("user", user);
+        cloned.save()
+            .then((clonedSaved) => {
+                res.send({
+                    newid: clonedSaved.id
+                });
+            }, (err) => {
+                res.send({ error : err});
+            });
+    }).catch(err => {
+        res.send({
+            error: err
+        });
+    });
 });
 
 app.use('/parse', parseServerAPI);
@@ -38,27 +67,24 @@ app.use('/parse', parseServerAPI);
 app.use('/plugins', express.static(path.resolve(process.cwd(), 'plugins')));
 
 
-
-if(IS_DEVELOPMENT) {
-    let users: {user: string; pass: string}[] = [];
+if (IS_DEVELOPMENT) {
+    let users: { user: string; pass: string }[] = [];
     if (DASHBOARD_AUTH) {
         const [user, pass]: string[] = DASHBOARD_AUTH.split(':');
         users = [{user, pass}];
     }
     const dashboard = ParseDashboard({
-        apps: [{
-            serverURL: '/parse',
-            appId: APP_ID,
-            masterKey: MASTER_KEY,
-            appName: 'parse-webplugin',
-        }],
-        users,
-    },
-    { allowInsecureHTTP: true });
+            apps: [{
+                serverURL: '/parse',
+                appId: APP_ID,
+                masterKey: MASTER_KEY,
+                appName: 'parse-webplugin',
+            }],
+            users,
+        },
+        {allowInsecureHTTP: true});
     app.use("/dashboard", dashboard);
 }
-
-
 
 app.listen(SERVER_PORT, () => console.log(
     `Notre serveur tourne en mode ${process.env.NODE_ENV || 'development'} sur http://localhost:${SERVER_PORT}`
